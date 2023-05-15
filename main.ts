58dadc620c82eca0be1a2ad4064575bb89136085
ryuichi usami <80155159+chateight@users.noreply.github.com>
2023-05-15 20:07:05 +0900

    MakeCodeプロジェクトの初期ファイル

---
+++ main.ts
@@ -0,0 +1,49 @@
+function fwd () {
+    pins.digitalWritePin(DigitalPin.P13, 1)
+    pins.digitalWritePin(DigitalPin.P15, 1)
+    pins.analogWritePin(AnalogPin.P14, 150)
+    pins.analogWritePin(AnalogPin.P16, 140)
+}
+function tr () {
+    pins.digitalWritePin(DigitalPin.P13, 1)
+    pins.digitalWritePin(DigitalPin.P15, 1)
+    pins.analogWritePin(AnalogPin.P14, 0)
+    pins.analogWritePin(AnalogPin.P16, 140)
+}
+function stop () {
+    pins.digitalWritePin(DigitalPin.P13, 1)
+    pins.digitalWritePin(DigitalPin.P15, 1)
+    pins.analogWritePin(AnalogPin.P14, 0)
+    pins.analogWritePin(AnalogPin.P16, 0)
+}
+function tl () {
+    pins.digitalWritePin(DigitalPin.P13, 1)
+    pins.digitalWritePin(DigitalPin.P15, 1)
+    pins.analogWritePin(AnalogPin.P14, 150)
+    pins.analogWritePin(AnalogPin.P16, 0)
+}
+let lv = 0
+let rv = 0
+let th = 300
+basic.forever(function () {
+    rv = pins.analogReadPin(AnalogPin.P0)
+    lv = pins.analogReadPin(AnalogPin.P2)
+    if (Math.abs(rv - lv) < th) {
+        basic.showNumber(0)
+        fwd()
+        basic.pause(100)
+        stop()
+    } else {
+        if (rv < lv) {
+            basic.showNumber(1)
+            tr()
+            basic.pause(100)
+            stop()
+        } else {
+            basic.showNumber(2)
+            tl()
+            basic.pause(100)
+            stop()
+        }
+    }
+})
