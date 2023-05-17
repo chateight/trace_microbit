5f4cf0dda2df9e0052114cda5da354e05f345fd5
ryuichi usami <80155159+chateight@users.noreply.github.com>
2023-05-17 17:30:04 +0900

    to set P parameter

--- main.ts
+++ main.ts
@@ -4,11 +4,11 @@ function fwd () {
     pins.analogWritePin(AnalogPin.P14, 160)
     pins.analogWritePin(AnalogPin.P16, 150)
 }
-function tr () {
+function tr (pow: number) {
     pins.digitalWritePin(DigitalPin.P13, 1)
     pins.digitalWritePin(DigitalPin.P15, 1)
     pins.analogWritePin(AnalogPin.P14, 0)
-    pins.analogWritePin(AnalogPin.P16, 150)
+    pins.analogWritePin(AnalogPin.P16, pow)
 }
 function stop () {
     pins.digitalWritePin(DigitalPin.P13, 1)
@@ -16,34 +16,36 @@ function stop () {
     pins.analogWritePin(AnalogPin.P14, 0)
     pins.analogWritePin(AnalogPin.P16, 0)
 }
-function tl () {
+function tl (pow: number) {
     pins.digitalWritePin(DigitalPin.P13, 1)
     pins.digitalWritePin(DigitalPin.P15, 1)
-    pins.analogWritePin(AnalogPin.P14, 160)
+    pins.analogWritePin(AnalogPin.P14, pow)
     pins.analogWritePin(AnalogPin.P16, 0)
 }
 let lv = 0
 let rv = 0
+let pow = 0
 let th = 100
+let P = 3
+let pb = 200
 basic.forever(function () {
     rv = pins.analogReadPin(AnalogPin.P0)
     lv = pins.analogReadPin(AnalogPin.P2)
+    pow = Math.abs(rv - lv) / (rv + lv) * P
     if (Math.abs(rv - lv) < th) {
         basic.showNumber(0)
         fwd()
-        basic.pause(200)
-        stop()
+        basic.pause(130)
     } else {
         if (rv < lv) {
             basic.showNumber(1)
-            tr()
+            tr(pb * pow)
             basic.pause(100)
-            stop()
         } else {
             basic.showNumber(2)
-            tl()
+            tl(pb * pow)
             basic.pause(100)
-            stop()
         }
     }
+    stop()
 })
