39bf77747ee663bfafc1db10bd226c258f91d002
ryuichi usami <80155159+chateight@users.noreply.github.com>
2023-05-18 19:15:30 +0900

    to remove led display since it's time consuming

--- main.ts
+++ main.ts
@@ -10,12 +10,6 @@ function tr (pow: number) {
     pins.analogWritePin(AnalogPin.P14, 0)
     pins.analogWritePin(AnalogPin.P16, pow)
 }
-function stop () {
-    pins.digitalWritePin(DigitalPin.P13, 1)
-    pins.digitalWritePin(DigitalPin.P15, 1)
-    pins.analogWritePin(AnalogPin.P14, 0)
-    pins.analogWritePin(AnalogPin.P16, 0)
-}
 function tl (pow: number) {
     pins.digitalWritePin(DigitalPin.P13, 1)
     pins.digitalWritePin(DigitalPin.P15, 1)
@@ -26,26 +20,19 @@ let lv = 0
 let rv = 0
 let pow = 0
 let th = 100
-let P = 3
-let pb = 200
+let P = 6
+let pb = 250
 basic.forever(function () {
     rv = pins.analogReadPin(AnalogPin.P0)
     lv = pins.analogReadPin(AnalogPin.P2)
-    pow = Math.constrain(Math.abs(rv - lv) / (rv + lv) * P, 0.6, 2)
+    pow = pb * Math.constrain(Math.abs(rv - lv) / (rv + lv) * P, 0.6, 2)
     if (Math.abs(rv - lv) < th) {
-        basic.showNumber(0)
         fwd()
-        basic.pause(130)
     } else {
         if (rv < lv) {
-            basic.showNumber(1)
-            tr(pb * pow)
-            basic.pause(100)
+            tr(pow)
         } else {
-            basic.showNumber(2)
-            tl(pb * pow)
-            basic.pause(100)
+            tl(pow)
         }
     }
-    stop()
 })
