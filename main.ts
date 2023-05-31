cb0aa9d7fea9f0be424bceaca9ac5514b43701e7
ryuichi usami <80155159+chateight@users.noreply.github.com>
2023-05-31 14:55:40 +0900

    to add logging to the flash memory

--- main.ts
+++ main.ts
@@ -10,6 +10,13 @@ function tr (pow: number) {
     pins.analogWritePin(AnalogPin.P14, 0)
     pins.analogWritePin(AnalogPin.P16, pow)
 }
+function log () {
+    datalogger.log(
+    datalogger.createCV("pow", pow),
+    datalogger.createCV("rv", rv),
+    datalogger.createCV("lv", lv)
+    )
+}
 function tl (pow: number) {
     pins.digitalWritePin(DigitalPin.P13, 1)
     pins.digitalWritePin(DigitalPin.P15, 1)
@@ -20,12 +27,17 @@ let lv = 0
 let rv = 0
 let pow = 0
 let th = 100
-let P = 4
+let P = 3
 let pb = 130
+datalogger.setColumnTitles(
+"pow",
+"rv",
+"lv"
+)
 basic.forever(function () {
     rv = pins.analogReadPin(AnalogPin.P0)
     lv = pins.analogReadPin(AnalogPin.P2)
-    pow = pb * Math.constrain(Math.abs(rv - lv) / (rv + lv) * P, 0.6, 2)
+    pow = pb * Math.constrain(Math.abs(rv - lv) / (rv + lv) * P, 1, 2.5)
     if (Math.abs(rv - lv) < th) {
         fwd()
     } else {
@@ -35,5 +47,5 @@ basic.forever(function () {
             tl(pow)
         }
     }
-    basic.pause(20)
+    log()
 })
